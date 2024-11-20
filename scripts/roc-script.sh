c55ed75b81fc700bfeb7269b7eb7f2a6a7fcbf87
Roc Lai <laipeng668@qq.com>
2024-11-21 07:40:57 +0800

    添加源码库以及稳定版分支

--- scripts/roc-script.sh
+++ scripts/roc-script.sh
@@ -12,7 +12,7 @@ sed -i "s/hostname='.*'/hostname='Roc'/g" package/base-files/files/bin/config_ge
 rm -rf feeds/packages/net/alist
 rm -rf feeds/luci/applications/luci-app-alist
 rm -rf feeds/packages/net/adguardhome
-# rm -rf feeds/packages/net/ariang
+rm -rf feeds/packages/net/ariang
 rm -rf package/emortal/luci-app-athena-led
 
 # Git稀疏克隆，只克隆指定目录到本地
@@ -28,7 +28,7 @@ function git_sparse_clone() {
 # Alist & AdGuardHome & 集客无线AC控制器 & Lucky & AriaNg & 雅典娜LED控制
 git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
 git_sparse_clone main https://github.com/kenzok8/small-package adguardhome luci-app-adguardhome
-# git_sparse_clone master https://github.com/immortalwrt/packages net/ariang 
+git_sparse_clone master https://github.com/immortalwrt/packages net/ariang 
 git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/lucky
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
